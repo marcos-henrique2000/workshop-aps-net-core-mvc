5a4d7aad28835ec7a9009446c5e32f3e84039b48
Marcos Marins <marcos-henrique2000@hotmail.com>
2019-12-03 09:48:08 -0300

    CRUD Scaffolding

---
+++ SalesWebMcv/Data/SalesWebMcvContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesWebMcv.Models
+{
+    public class SalesWebMcvContext : DbContext
+    {
+        public SalesWebMcvContext (DbContextOptions<SalesWebMcvContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMcv.Models.Department> Department { get; set; }
+    }
+}
