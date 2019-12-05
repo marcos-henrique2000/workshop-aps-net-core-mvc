10396a7aee4f3e04d9a4e58ecaa58872f2587d1e
Marcos Marins <marcos-henrique2000@hotmail.com>
2019-12-05 09:01:57 -0300

    Update Seller and custom service exception

--- SalesWebMcv/Services/SellerService.cs
+++ SalesWebMcv/Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using SalesWebMcv.Services.Exceptions;
 
 namespace SalesWebMcv.Services
 {
@@ -37,6 +38,23 @@ namespace SalesWebMcv.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if(!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }catch(DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException("Erro de acesso a dados! " + e.Message);
+            }
         }
 
     }
